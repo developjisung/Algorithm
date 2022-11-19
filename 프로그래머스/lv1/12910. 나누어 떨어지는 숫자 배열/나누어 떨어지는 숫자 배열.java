dc9fe8ddd5f365174b22a0324726efc7fa364a33
developjisung <117707115+developjisung@users.noreply.github.com>
2022-11-19 17:02:26 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: ., Memory: undefined -BaekjoonHub

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -1,29 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Comparator;
+import java.util.*;
 
 class Solution {
-    public int[] solution(int[] arr, int divisor) {
-       List<Integer> result = new ArrayList<>();
-
+    public ArrayList<Integer> solution(int[] arr, int divisor) {
+       ArrayList<Integer> result = new ArrayList<>();
 
         for(int i = 0; i < arr.length; i++){
             if(arr[i] % divisor == 0){
                 result.add(arr[i]);
-
             }
         }
+        
+        Collections.sort(result);
         
         if(result.size() == 0){
             result.add(-1);
         }
-        result.sort(Comparator.naturalOrder());
         
-        int[] f = new int[result.size()];
-        for(int i = 0; i< result.size(); i++){
-            f[i] = result.get(i);
-        }
-
-        return f;
+        return result;
     }
 }
