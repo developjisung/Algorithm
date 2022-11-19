ead0c79a943dc04537b2c90e173103168c487767
developjisung <117707115+developjisung@users.noreply.github.com>
2022-11-19 18:01:50 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.15 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -5,8 +5,8 @@ public class Solution {
         String[] arr = Integer.toString(n).split("");       // 주어진 값을 String으로 변환 후, split
         int result = 0;
 
-        for(int i = 0; i< arr.length; i++){                 // 각 자릿수 더하기
-            result = result + Integer.parseInt(arr[i]);
+        for (String s : arr) {                       // 각 자릿수 더하기
+            result = result + Integer.parseInt(s);
         }
 
         return result;
