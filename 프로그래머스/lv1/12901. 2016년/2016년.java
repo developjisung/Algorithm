d053491104955ce362651da4ed05abf5673f32a1
developjisung <117707115+developjisung@users.noreply.github.com>
2022-11-19 15:07:05 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -1,16 +1,17 @@
 class Solution {
     public String solution(int a, int b) {
         String today = "";
-        String[] day = {"THU", "FRI", "SAT", "SUN", "MON","TUE", "WED"};        // 요일 이름
-        int[] month = {0,31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};         // 달별 일 수
+        String[] day = {"THU", "FRI", "SAT", "SUN", "MON","TUE", "WED"};            // 요일 이름
+        int[] month = {0,31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};           // 달별 일 수
         int total = 0;
 
-        for(int i = 0; i < a; i++){
+        for(int i = 0; i < a; i++){         // 달에 따른 일수 계산
             total = total + month[i];
         }
-        total = total + b;
+        total = total + b;                  // 달에 따른 일수 + 해당 달의 일수
 
-        today = day[(total % 7)];
+        today = day[(total % 7)];           // % 연산을 통해 어떤 요일인지 계산
+                                            // 0 : THU  1 : FRI  ....
         return today;
     }
 }
