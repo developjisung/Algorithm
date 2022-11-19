4c5e6fecb32967c0d0b7b715f330a864a29a076c
developjisung <117707115+developjisung@users.noreply.github.com>
2022-11-19 19:33:53 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.60 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -2,26 +2,26 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr) {
-       int[] result = new int[arr.length - 1];
-        int[] temp = arr.clone();
-        int count = 0;
+       int[] result = new int[arr.length - 1];     // 최솟값을 제외한 나머지 값들을 저장할 결과 배열
+        int[] temp = arr.clone();                   // 정렬한 후, 최솟값을 찾기 위한 배열 
+        int count = 0;                              // 최솟값을 찾은 후, 나머지 값들을 result 배열에 넣기 위한 count 
+        int min_value = 0;
+        
+        Arrays.sort(temp);                          // 오름차순 정렬                      
+        min_value = temp[0];                        // 최솟값 저장
 
-        Arrays.sort(temp);
-        int min_value = temp[0];
-
-        for(int i = 0; i< arr.length; i++){
+        for(int i = 0; i< arr.length; i++){         // min_value를 제외한 나머지 값 result 배열 입력
             if(arr[i] != min_value){
                 result[count] = arr[i];
                 count = count + 1;
             }
         }
-        
-        if(result.length == 0){
-            int[] zero_result = {-1};
 
+        if(result.length == 0) {                     // result에 아무것도 없으면 -1 입력
+            int[] zero_result = {-1};
             return zero_result;
-        }else{
-            return result;
         }
+        
+        return result;
     }
 }
