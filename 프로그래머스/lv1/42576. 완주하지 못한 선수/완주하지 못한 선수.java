368c7b48962ea61b32fc2822bb99feaddae7fd0a
developjisung <117707115+developjisung@users.noreply.github.com>
2022-11-19 17:52:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 302.85 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,20 +2,22 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+       Arrays.sort(participant);           // participant sort
+        Arrays.sort(completion);            // completion sort
+        String result = "";
 
         for(int i = 0; i < participant.length; i++){
-            if(i == completion.length){
-                return participant[i];
+            if(i == completion.length){             // completion 배열의 끝까지 순회했다면
+                result = participant[i];            // participant 배열의 마지막 값을 저장
+                break;
             }
 
-            if(participant[i].equals(completion[i]) == false){
-                return participant[i];
+            if(!participant[i].equals(completion[i])){  // 두 배열의 값이 동일하지 않다면
+                result =  participant[i];                       // participant 배열의 값을 저장
+                break;
             }
         }
-        
-        return "hello";
-        
+
+        return result;
     }
 }
