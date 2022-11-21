2d08dd602c1294a5cbbbefa3eaf37fe2b1c9be30
developjisung <117707115+developjisung@users.noreply.github.com>
2022-11-21 15:22:12 +0900

    [level 1] Title: 모의고사, Time: 1.43 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,45 +1,28 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 class Solution {
-      public static ArrayList<Integer> solution(int[] answers) {
+    public int[] solution(int[] answers) {
         int[] number1 = {1,2,3,4,5};
         int[] number2 = {2,1,2,3,2,4,2,5};
         int[] number3 = {3,3,1,1,2,2,4,4,5,5};
-        int[] temp;
         int[] count  = new int[3];
-        int max_correct = 0;
-        ArrayList<Integer> result = new ArrayList<>();
-
+        
         for(int i = 0; i< answers.length; i++){
             if(number1[i % 5] == answers[i]){
                 count[0] = count[0] + 1;
             }
-
+            
             if(number2[i % 8] == answers[i]){
                 count[1] = count[1] + 1;
             }
-
+            
             if(number3[i % 10] == answers[i]){
                 count[2] = count[2] + 1;
             }
         }
-
-        temp = count.clone();
-        Arrays.sort(temp);
-
-        for(int i = 0; i< temp.length; i++){
-            if(max_correct < count[i]){
-                max_correct = count[i];
-            }
+        
+        for(int i = 0; i< count.length; i++){
+            System.out.println(count[i]);
         }
-
-        for(int i  = 0; i< count.length; i++){
-            if(max_correct == count[i]){
-                result.add(i+1);
-            }
-        }
-
-        return result;
+        
+        return count;
     }
 }
