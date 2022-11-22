1475de871973ca6b02087a5e96cabbfb7e28dfaa
developjisung <117707115+developjisung@users.noreply.github.com>
2022-11-22 11:18:53 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 10.81 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -2,35 +2,49 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        int str_length = s.split("").length;
-        List<Character> low_alpha = new ArrayList<>();
-        List<Character> upper_alpha = new ArrayList<>();
+//         int str_length = s.split("").length;
+//         List<Character> low_alpha = new ArrayList<>();
+//         List<Character> upper_alpha = new ArrayList<>();
 
-        for(int i = 0; i <str_length; i++ ){
-            if(s.charAt(i) >= 65 && s.charAt(i) <97){
-                upper_alpha.add(s.charAt(i));
-            }
-            else{
-                low_alpha.add(s.charAt(i));
-            }
-        }
+//         for(int i = 0; i <str_length; i++ ){
+//             if(s.charAt(i) >= 65 && s.charAt(i) <97){
+//                 upper_alpha.add(s.charAt(i));
+//             }
+//             else{
+//                 low_alpha.add(s.charAt(i));
+//             }
+//         }
 
-        Collections.sort(low_alpha);
-        Collections.sort(upper_alpha);
+//         Collections.sort(low_alpha);
+//         Collections.sort(upper_alpha);
         
-        Collections.reverse(low_alpha);
-        Collections.reverse(upper_alpha);
+//         Collections.reverse(low_alpha);
+//         Collections.reverse(upper_alpha);
 
+//         String result = "";
+
+//         for(int i = 0; i < low_alpha.size(); i++){
+//             result = result + low_alpha.get(i);
+//         }
+
+//         for(int i = 0; i <upper_alpha.size(); i++){
+//             result = result + upper_alpha.get(i);
+//         }
+
+//         return result;
+        int str_length = s.split("").length;
         String result = "";
+        int[] temp = new int[str_length];
 
-        for(int i = 0; i < low_alpha.size(); i++){
-            result = result + low_alpha.get(i);
+        for(int i = 0; i<str_length; i++){
+            temp[i] = (int)s.charAt(i);
         }
+        Arrays.sort(temp);
 
-        for(int i = 0; i <upper_alpha.size(); i++){
-            result = result + upper_alpha.get(i);
+        for(int i = str_length - 1; i >=0; i--){
+            result = result + (char)temp[i];
         }
-
+        System.out.println(result);
         return result;
     }
 }
